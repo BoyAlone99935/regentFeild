42859e016fdb1d31a82f2733db97aaa55ce275b9
BoyAlone99935 <kingsleychijioke266@gmail.com>
2025-12-11 16:03:51 +0100

    Updated package.json

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://funny-faun-1b03ad.netlify.app",
+    origin: "https://spiffy-melba-133240.netlify.app",
     methods: ["GET", "POST"],
   },
 });
