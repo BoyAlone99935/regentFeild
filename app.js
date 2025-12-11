cbc52b4f09361e99e7bf4509038a8c5d42c5a1d2
BoyAlone99935 <kingsleychijioke266@gmail.com>
2025-12-11 17:52:36 +0100

    Added backend code

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://incomparable-piroshki-4459f4.netlify.app/",
+    origin: "http://localhost:5173/",
     methods: ["GET", "POST"],
   },
 });
