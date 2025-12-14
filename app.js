a8b87f753dd4b7afcb5dcfefba7be583469e3a31
BoyAlone99935 <kingsleychijioke266@gmail.com>
2025-12-14 15:43:00 +0100

    Initial commit

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://heartfelt-lokum-541b33.netlify.app/",
+    origin: "http://localhost:5173/",
     methods: ["GET", "POST"],
   }, 
 });
