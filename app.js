a7f92a39e4679d117ad79d1ea0417064171e93cd
BoyAlone99935 <kingsleychijioke266@gmail.com>
2025-12-14 19:09:04 +0100

    Initial commit

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173/",
+    origin: "https://resonant-faloodeh-bb97eb.netlify.app/",
     methods: ["GET", "POST"],
   }, 
 });
