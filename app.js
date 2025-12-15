56f43c15d18d1e335924129a96189e29c28bdbbc
BoyAlone99935 <kingsleychijioke266@gmail.com>
2025-12-15 02:01:13 +0100

    Initial commit

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173/",
+    origin: "https://sunny-sundae-da5507.netlify.app",
     methods: ["GET", "POST"],
   }, 
 });
