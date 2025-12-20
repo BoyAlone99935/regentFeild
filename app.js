72ea234e89aeb88d09dc4e2acaf6916332d1a7f8
BoyAlone99935 <kingsleychijioke266@gmail.com>
2025-12-20 11:59:51 +0100

    Initial commit: CORS changes

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://polite-snickerdoodle-f968b2.netlify.app/",
+    origin: " http://localhost:5173/",
     methods: ["GET", "POST"],
   }, 
 });
