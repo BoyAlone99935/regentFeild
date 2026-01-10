8b3228455637518884cebda0903126c4e2a97f17
BoyAlone99935 <kingsleychijioke266@gmail.com>
2026-01-10 01:54:06 +0100

    Initial commit

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://delicate-crisp-aa5b63.netlify.app",
+    origin: "https://effulgent-stroopwafel-625257.netlify.app",
     methods: ["GET", "POST"],
   }, 
 });
